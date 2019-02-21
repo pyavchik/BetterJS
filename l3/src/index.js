40eb2c978e4a9246757c867d968e171841792860
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-21 23:18:21 +0200

    l4 added

---
+++ l3/src/index.js
@@ -0,0 +1,38 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello Parcel!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
+
+
+const makePerson = ({firstName, age, job, lastName}) => {
+    return {
+        name: firstName + ' ' + lastName,
+        age,
+        job,
+        lastName
+    }
+};
+
+// const tempName = {
+//     name: 'Scot',
+//     lastName: 'Tolinsky',
+//     age: '34',
+//     job: 'web disigner',
+// };
+
+const dev = makePerson({
+    firstName: 'Scot',
+    lastName: 'Tolinsky',
+    age: '34',
+    job: 'web disigner',
+});
+
+console.log(dev);
+
+const { name, ...rest } = dev;
+console.log(name);
+console.log(rest);
