69c99a09cd93656e1234981fcb0cdb47c1217a2f
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-24 21:20:50 +0200

    l9 added

---
+++ l9/src/index.js
@@ -0,0 +1,21 @@
+// Generate random color
+// // RGB
+// Apply to dom
+// update color on event
+
+const generateColorValue = () => Math.floor(Math.random() * 256);
+const createColor = () => {
+    const red = generateColorValue();
+    const green = generateColorValue();
+    const blue = generateColorValue();
+    return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const applyColorToBody = color => document.body.style.backgroundColor = color;
+
+const addRandomColorToBackgroun = () => {
+    const color = createColor();
+    return applyColorToBody(color);
+}
+
+addRandomColorToBackgroun();
