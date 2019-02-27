2190c4451f23ce2e2eef59483be4434caeee19a8
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-27 21:17:11 +0200

    l22 added

---
+++ l22/src/index.js
@@ -0,0 +1,13 @@
+(async () => {
+    try {
+        const SEARCH_QUERY = 'Javascript';
+        const SEARCH_URL = `http://openlibrary.org/search.json?q=`;
+        const API_URL = `${SEARCH_URL}${SEARCH_QUERY}`;
+
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error.message);
+    }
+})();
